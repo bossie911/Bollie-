30cf56d8f6045cf06eab3b8251be53f29ab76804
Laurens <Laurenscnossen@gmail.com>
2021-04-14 14:33:59 +0200

    Business hub buttons working

--- app/src/main/java/com/example/mdp_bollie/ui/business/BusinesHubFragment.kt
+++ app/src/main/java/com/example/mdp_bollie/ui/business/BusinesHubFragment.kt
@@ -5,32 +5,24 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.findNavController
 import androidx.navigation.fragment.findNavController
 import com.example.mdp_bollie.R
 import com.example.mdp_bollie.databinding.FragmentBusinesHubBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [BusinesHubFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class BusinesHubFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var _binding: FragmentBusinesHubBinding? = null
     private val binding get() = _binding!!
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
     }
 
     override fun onCreateView(
-            inflater: LayoutInflater, container: ViewGroup?,
-            savedInstanceState: Bundle?
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
     ): View {
         _binding = FragmentBusinesHubBinding.inflate(inflater, container, false)
         return binding.root
@@ -38,14 +30,18 @@ class BusinesHubFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-//        binding.btnBestellingen2.setOnClickListener {
-//            findNavController().navigate(R.id.action_businesHubFragment_to_businessBestellingenFragment2)
-//        }
+        val navController = findNavController()
+        binding.btnBestellingen2.setOnClickListener {
+            navController.navigate(R.id.action_navigation_businessHub_to_businessBestellingenFragment)
+        }
         binding.btnKlantvragen2.setOnClickListener {
-
+            navController.navigate(R.id.action_navigation_businessHub_to_businessKlantvragenFragment)
         }
         binding.btnRetouren2.setOnClickListener {
-
+            navController.navigate(R.id.action_navigation_businessHub_to_businessBestellingenFragment)
+        }
+        binding.btnOmzet2.setOnClickListener {
+            navController.navigate(R.id.action_navigation_businessHub_to_businessFinancienFragment)
         }
     }
 }
